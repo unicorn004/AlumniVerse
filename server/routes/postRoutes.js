8860bc2bb41fc81398743a70abc8e40984163420
adityatiwari212 <aditya.b.tiwari2005@gmail.com>
2025-04-18 16:21:23 +0530

    min

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.get('/', protect, getAllPosts);
 router.post('/', protect, isAlumni, upload.single('image'), createPost); // Image upload via Cloudinary
-router.put('/:id/like', protect, likePost);
-router.post('/:id/comment', protect, commentOnPost);
+router.put('/like/:id', protect, likePost);
+router.post('/comment/:id', protect, commentOnPost);
 
 module.exports = router;
