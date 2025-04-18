9c837a8bbd2ca293633d1e25a8a1f6b1e5c5257d
adityatiwari212 <aditya.b.tiwari2005@gmail.com>
2025-04-18 20:33:51 +0530

    add some routes

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -16,6 +16,8 @@ exports.protect = async (req, res, next) => {
     
     req.user = await User.findById(decoded.id).select('-passwordHash');
     if (!req.user) return res.status(401).json({ message: 'User not found' });
+    console.log("success in protect");
+    
     next();
   } catch (err) {
     console.error('JWT error:', err.message);
