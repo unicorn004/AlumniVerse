79df2f9f3f6ed2502b94a3de42e340dd1e5270fc
Tanmay <tanmaysinkar2021@gmail.com>
2025-04-18 13:43:35 +0530

    server

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(process.env.PORT || 5000, () =>
-      console.log(`🚀 Server running on http://localhost:${process.env.PORT || 5000}`)
+      console.log(`Server running on http://localhost:${process.env.PORT || 5000}`)
     );
   }).catch((err) => {
     console.error('MongoDB connection error:', err.message);
