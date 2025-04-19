65268e5171bc6297b0c234f733a882d114bf1742
adityatiwari212 <aditya.b.tiwari2005@gmail.com>
2025-04-20 00:59:02 +0530

    trying to fix

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ALLOWED_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ALLOWED_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
   },
