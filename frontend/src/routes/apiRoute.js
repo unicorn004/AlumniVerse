13fe362a40665a892e5b48b84a8aa49c59830ddc
Aryan_Kanyawar <aryankan2020@gmail.com>
2025-04-20 21:33:09 +0530

    fix

--- frontend/src/routes/apiRoute.js
+++ frontend/src/routes/apiRoute.js
@@ -1,5 +1,5 @@
-//export const API_BASE_URL = "https://alumniverse.onrender.com";
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://alumniverse.onrender.com";
+//export const API_BASE_URL = "http://localhost:5000";
 
 export const API_ROUTES = {
   REGISTER: `${API_BASE_URL}/api/auth/register`,
